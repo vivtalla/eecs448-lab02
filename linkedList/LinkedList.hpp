c7f9f3f6930dac1527f4e19e5aa402297d10bd58
Vivek Tallavajhala <vivtalla@ku.edu>
2017-09-04 19:05:13 -0500

    Fix LinkedList.hpp search method
    Updated search method such that it returns true if value is found
    and false otherwise.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,11 +37,17 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-
-	/** TODO
-		Fix this method
-	*/
-
+	while(temp != nullptr)
+	{
+		if(temp -> getValue() == value)
+		{
+			isFound = true;
+		}
+		else
+		{
+			temp = temp->getNext();
+		}
+	}
 	return(isFound);
 }
 
